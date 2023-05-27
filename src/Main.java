634e3a3da39421161dad99440a747c16714064db
DavTheDev20 <reiddavin@outlook.com>
2023-05-27 19:50:07 -0400

    Add additional iterations due to speed increase after code removal

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        int testIterations = 10_000_000;
+        int testIterations = 100_000_000;
         benchmarkTest(testIterations);
 
     }
